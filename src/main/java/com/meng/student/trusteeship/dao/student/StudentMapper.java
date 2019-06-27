8984f4f60177ea5f824cd6cc5c49a63a732c3adb
mistletoe <757926529@qq.com>
2019-06-27 21:24:27 +0800

    提交  service代码

--- src/main/java/com/meng/student/trusteeship/dao/student/StudentMapper.java
+++ src/main/java/com/meng/student/trusteeship/dao/student/StudentMapper.java
@@ -1,6 +1,10 @@
 package com.meng.student.trusteeship.dao.student;
 
+import com.meng.student.trusteeship.entity.student.pojo.RegistrationIntention;
+import com.meng.student.trusteeship.entity.student.pojo.Student;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
 
 /**
  * 学生dao
@@ -10,4 +14,9 @@ import org.apache.ibatis.annotations.Mapper;
 public interface StudentMapper {
 
 
+    Map<String, Object> listAllStudentByCondition(Map<String, Object> queryMap);
+
+    void insertStudent(Student student);
+
+    void insertRegistrationIntention(RegistrationIntention registrationIntention);
 }
